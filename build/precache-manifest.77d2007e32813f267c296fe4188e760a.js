8537d486004c38a87d0cf4516c75ecf54b1c0b8f
Jhayla Young <jhayla.young@gmail.com>
2020-02-25 21:11:47 -0600

    deploying using npm run deploy

---
+++ build/precache-manifest.77d2007e32813f267c296fe4188e760a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd79694d8f56580a16340b2a7bf7753",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "6e2e45937a6531e77f12",
+    "url": "/click-game/static/css/main.20e1d9aa.chunk.css"
+  },
+  {
+    "revision": "646a75c58be7259d17ba",
+    "url": "/click-game/static/js/2.8c5b1c2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/click-game/static/js/2.8c5b1c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2e45937a6531e77f12",
+    "url": "/click-game/static/js/main.f41c986f.chunk.js"
+  },
+  {
+    "revision": "e2801d1c9c1b8cfc7a8f",
+    "url": "/click-game/static/js/runtime-main.410183ed.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25813072e24898542fece6ada5db16b8.js"
+  "/click-game/precache-manifest.77d2007e32813f267c296fe4188e760a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/click-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
